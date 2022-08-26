13f6ba3dd5269bbb714a01439292d2997f8cf8b2
Geli Yang <gyang5@villanova.edu>
2022-08-26 13:16:26 -0400

    parent maven project

---
+++ microservices_parent/inventory-service/src/main/java/com/microservices/inventoryservice/service/InventoryService.java
@@ -0,0 +1,29 @@
+package com.microservices.inventoryservice.service;
+
+
+import com.microservices.inventoryservice.model.Inventory;
+import com.microservices.inventoryservice.repository.InventoryRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+public class InventoryService {
+    private InventoryRepository inventoryRepository;
+
+    public InventoryService() {
+    }
+
+    public InventoryService(InventoryRepository inventoryRepository) {
+        this.inventoryRepository = inventoryRepository;
+    }
+
+    @Transactional(readOnly = true)
+    public boolean isInStock(String skuCode){
+        Optional<Inventory> currentInventory = inventoryRepository.findBySkuCode(skuCode);
+        return currentInventory.isPresent();
+    }
+
+
+}
---
+++ microservices_parent/product-service/src/main/java/com/geli/productservice/service/ProductService.java
@@ -0,0 +1,46 @@
+package com.geli.productservice.service;
+
+import com.geli.productservice.dto.ProductRequest;
+import com.geli.productservice.dto.ProductResponse;
+import com.geli.productservice.model.Product;
+import com.geli.productservice.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public void createProduct(ProductRequest productRequest) {
+        Product product = Product.builder()
+                .name(productRequest.getName())
+                .description(productRequest.getDescription())
+                .price(productRequest.getPrice())
+                .build();
+
+        productRepository.save(product);
+    }
+
+    public List<ProductResponse> getAllProducts() {
+        List<Product> products = productRepository.findAll();
+
+        return products.stream().map(this::mapToProductResponse).toList();
+    }
+
+    private ProductResponse mapToProductResponse(Product product) {
+        return ProductResponse.builder()
+                .id(product.getId())
+                .name(product.getName())
+                .description(product.getDescription())
+                .price(product.getPrice())
+                .build();
+    }
+}
