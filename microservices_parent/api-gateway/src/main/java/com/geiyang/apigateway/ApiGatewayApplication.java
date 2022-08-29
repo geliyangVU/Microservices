24c086a821576e55ea05a043b4b1f7f735f9325c
Geli Yang <gyang5@villanova.edu>
2022-08-29 17:14:39 -0400

    configured eureka service for ApiGateway, add product service route

--- microservices_parent/api-gateway/src/main/java/com/geiyang/apigateway/ApiGatewayApplication.java
+++ microservices_parent/api-gateway/src/main/java/com/geiyang/apigateway/ApiGatewayApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApiGateWayApplication {
+public class ApiGatewayApplication {
     public static void main(String[] args){
-        SpringApplication.run(ApiGateWayApplication.class, args);
+        SpringApplication.run(ApiGatewayApplication.class, args);
     }
 }
