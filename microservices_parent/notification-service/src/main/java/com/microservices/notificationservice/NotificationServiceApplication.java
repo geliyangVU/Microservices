548f0f4da61d9fc7b8eedca455be7eb7d58c6e37
Geli Yang <gyang5@villanova.edu>
2022-09-03 13:36:56 -0400

    add Consumer Model for Kafka supplier

--- microservices_parent/notification-service/src/main/java/com/microservices/notificationservice/NotificationServiceApplication.java
+++ microservices_parent/notification-service/src/main/java/com/microservices/notificationservice/NotificationServiceApplication.java
@@ -1,8 +1,11 @@
 package com.microservices.notificationservice;
 
+import org.apache.kafka.clients.consumer.Consumer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.messaging.Message;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -11,4 +14,16 @@ public class NotificationServiceApplication {
         SpringApplication.run(NotificationServiceApplication.class, args);
     }
 
+
+
+    @Bean
+    public Consumer<Message<OrderDto>> notificationEventSupplier() {
+        return message -> {
+            try {
+                new EmailSender().sendEmail(message.getPayload());
+            } catch (InterruptedException e) {
+                throw new RuntimeException("Something went wrong while sending email");
+            }
+        };
+    }
 }
